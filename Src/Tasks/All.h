1d4e7d40a1d8d7dab16476a0711d1e96858646e6
igorpieniek <46819243+igorpieniek@users.noreply.github.com>
2019-03-29 20:14:09 +0100

    Add files via upload

---
+++ Src/Tasks/All.h
@@ -0,0 +1,29 @@
+/*
+ * All.h
+ *
+ *  Created on: 27.03.2019
+ *      Author: Igor
+ */
+
+#ifndef TASKS_ALL_H_
+#define TASKS_ALL_H_
+
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "cmsis_os.h"
+
+void All_begin(void);
+
+//extern osThreadId GyroTaskHandle; np
+// trzeba dac takie zadania
+
+#ifdef __cplusplus
+ }
+
+#endif
+
+
+#endif /* TASKS_ALL_H_ */
